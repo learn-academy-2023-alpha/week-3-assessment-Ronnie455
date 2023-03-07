5fb691c7e7e3b67813e48ff48e66c79ea3a3472d
Ronnie455 <ronniemaynard4@gmail.com>
2023-03-07 00:36:23 -0800

    completed week 3 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,58 +10,210 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+//1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+ 
+// Pseudocode:
+// Input - create a working test using a describe function using the provided variables
+// Output - create a function that takes in a number and returns an array length containing the numbers of the Fibonacci sequence.
+// Created a working test by using "fibArray" as my describe variable. I then created a function using the variables proveded. I initialized the array "result" by using first two numbers in the array. It then iterates through the remaining indexes by adding the sum of the previous two elements. The function then returns the result of the array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+// const fibLength1 = 6;
+// const fibLength2 = 10;
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// describe("fibArray", () => {
+//   it("returns an array of length n containing the Fibonacci sequence", () => {
+//     expect(fibArray(fibLength1)).toEqual([1, 1, 2, 3, 5, 8]);
+//     expect(fibArray(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+//   });
+// });
+  
+// //TypeError: fibLength1 is not a function
 
-// b) Create the function that makes the test pass.
+// // Expected output: [1, 1, 2, 3, 5, 8]
+
+   
+
+// // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+// // b) Create the function that makes the test pass.
+// const fibArray = (num) => {
+//   const result = [1, 1];
+
+//   for (let i = 2; i < num; i++) {
+//     result.push(result[i - 2] + result[i - 1]);
+//   }
+
+//   return result;
+// };
+// console.log(fibArray([fibLength1]))
+// console.log(fibArray([fibLength2]));
+
+
+// [ 1, 1, 2, 3, 5, 8 ]
+
+// at Object.log (code-challenges.test.js:45:9)
+
+// console.log
+// [
+// 1,  1,  2,  3,  5,
+// 8, 13, 21, 34, 55
+// ]
+
+// at Object.log (code-challenges.test.js:46:9)
+
+// PASS  ./code-challenges.test.js
+// fibArray
+// ✓ returns an array of length n containing the Fibonacci sequence (3 ms)
+
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
+// Pseudocode: 
+// input - create a working test with provided variable values. After working test is working create a function
+//output - create a function that takes in an object and returns an array of the values sorted from least to greatest
+// created a test with the variable "sortObject"
+//  created function using sortObject variable
+// in the function sortObject, I will make the parameter obj
+// use object.values to iterate through the loop finding the indexes of the numbers in the arrays
+// I will need to return the sorted arrays from least to greatest using object.values and then sort the indexes using .sort them in order from least to greatest by using the ((a, b) => a - b) method
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
+// describe("sortObject", () => {
+//   it("sorts the array of the values from least to greatest", () => {
+// const studyMinutesWeek1 = {
+//   sunday: 90,
+//   monday: 30,
+//   tuesday: 20,
+//   wednesday: 15,
+//   thursday: 30,
+//   friday: 15,
+//   saturday: 60
+ 
+// }
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
+// // // Expected output: [15, 15, 20, 30, 30, 60, 90]
 
-// b) Create the function that makes the test pass.
+// const studyMinutesWeek2 = {
+//   sunday: 100,
+//   monday: 10,
+//   tuesday: 45,
+//   wednesday: 60,
+//   thursday: 20,
+//   friday: 15,
+//   saturday: 65
+// }
+// // Expected output: [10, 15, 20, 45, 60, 65, 100]
+
+
+//   expect(sortObject(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90]),
+//   expect(sortObject(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+//   console.log(sortObject(studyMinutesWeek1));
+// console.log(sortObject(studyMinutesWeek2));
+// })
+//   })
+
+
+// // b) Create the function that makes the test pass.
+
+// const sortObject = (obj) => {
+//   return Object.values(obj).sort((a, b) => a - b);
+//}
+
+//  [
+//   15, 15, 20, 30,
+//   30, 60, 90
+// ]
+
+//   at Object.log (code-challenges.test.js:101:11)
+
+// console.log
+// [
+//   10, 15,  20, 45,
+//   60, 65, 100
+// ]
+
+//   at Object.log (code-challenges.test.js:102:9)
+
+// PASS  ./code-challenges.test.js
+// sortObject
+// ✓ sorts the array of the values from least to greatest (19 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+// Pseudocode:
+// input - create a test using a desribe function. Once the test works create a function
+// output - create a function that takes in an array and returns array of the accumulating sum. An empty array should return an empty array.
+
+// Create a test with the variable "accumlatingSum" and use the provided variables as the expect arguments
+// create a function using my variable accumalatingSum
+// Set a parameter in the function called arr
+// declare variable called result using let. result will return the results of the function into an array.
+// declare sum variable using let = 0
+// create a for loop that iterates thhrough  the array starting at index 0
+// create addition assignment for sum and array index
+// result.push(sum) 
+// return result which will return the accumalated sums in an array
+// console.log to see results in terminal
+
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+// const accountTransactions1 = [100, -17, -23, -9]
+// // Expected output: [100, 83, 60, 51]
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
+// const accountTransactions2 = [250, -89, 100, -96]
+// // Expected output: [250, 161, 261, 165]
 
-const accountTransactions3 = []
-// Expected output: []
+// const accountTransactions3 = []
+// // Expected output: []
+// describe("accumulatingSum", () => {
+//   it("returns an array of the accumulating sum. an empty array should return an empty array", () => {
+//     expect(accumulatingSum(accountTransactions1)).toEqual([100, 83, 60, 51]),
+//     expect(accumulatingSum(accountTransactions2)).toEqual([250, 161, 261, 165]),
+//     expect(accumulatingSum(accountTransactions3)).toEqual([])
+//   })
+// })
+// // b) Create the function that makes the test pass.
+// const accumulatingSum = (arr) => {
+//   let result = [];
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//     result.push(sum);
+//   }
+//   return result;
+// };
+// console.log(accumulatingSum(accountTransactions1));
+// console.log(accumulatingSum(accountTransactions2));
+// console.log(accumulatingSum(accountTransactions3));
 
-// b) Create the function that makes the test pass.
+// [ 100, 83, 60, 51 ]
+
+//       at Object.log (code-challenges.test.js:166:9)
+
+//   console.log
+//     [ 250, 161, 261, 165 ]
+
+//       at Object.log (code-challenges.test.js:167:9)
+
+//   console.log
+//     []
+
+//       at Object.log (code-challenges.test.js:168:9)
+
+//  PASS  ./code-challenges.test.js
+//   accumulatingSum
+//     ✓ returns an array of the accumulating sum. an empty array should return an empty array (3 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX allows us to implement Javascript within HTML using component. The syntax difference comes from the curly braces implemented within the HTML that allows us to call upon Javascript functions.
 
-Researched answer:
+Researched answer: JavaScript Syntac Extension is an extension that allows you to write similar javascript code similar to HTML. Curly braces are used to pass values as props to the component in HTML. <></> fragment tags is a way to list a group of children elements without adding extra nodes to DOM. Fragments allow you to return multiple elements from a component without needing to wrap them in a parent element.
 
 1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: I would have to go back and research further what yarn is specifically, but I do know that yarn installs packages called yarn.lock and packages.json and it's necessary to install when working with multiple files or folder for the same project.
 
-Researched answer:
+Researched answer: Yarn is a package manager for Javascript that was developed by Facebook. It allows you to manage and install packages of reusable code that is used in Javascript. A major feature of Yarn is its ability to download and install packages at the same time. Another feature of Yarns is the workspaces feature. This allows you to manage multiple different packages in a sigle repository which can then be published together.
 
-3. What is a React component?
+3. What is a React component? 
 
-Your answer:
+Your answer: React component uses Javascript that can be exported and imported into different locations including HTML and are called upon when using the correct Javascript syntax. As a new devoloper I haven't worked with components enough to fully understand the ins and outs of components and their html and css implications. When the interview is finished would you mind giving me research tips so I can better understand components?
 
-Researched answer:
+Researched answer: A component is a modular, reusable piece of code that wraps a specific functionality. They can be a class or function component. Class components use ES6 classes. ES6 classes are more complex. They're designed to be used in multiple places throughout an application. React components use JSX syntax.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State uses hooks. Props stands for properties and acts like parameters. I can't elaborate fully on states and props as I don't have a full understanding of them yet. I would have to do more research on them. Would you mind pointing me in the right direction for further learning them as well as react components?
 
-Researched answer:
+Researched answer: State is an internal data of component that can be changed and updated. state is managed using 'useState' which is a hook. it allows components to store and update data within their scope. Props are the data that's passed into a component from the parent component. props cannot be modified within the component. They are strings. 
 
 5. What is the DOM?
 
-Your answer:
+Your answer: DOM stands for Document Object Model. DOM is everything that is interactable with a user. Like clicking a button, hyperlink, inputing text.
 
-Researched answer:
+Researched answer: DOM is a tree structure of the represented html elements in a webpage or app. DOM is the User interface of the application. It allows developers to modify content using Javascript. 
+
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: A div tag divides code within a code block allowing to call multiple different related elements to perform a specific outcome. I would need to research what a span tag is, but I think it operates similarly to a div tag spanning the entire command.
 
-Researched answer:
+Researched answer: A div tag is used to divide a section of the web page. it's a container element to group related elements and implements the styling of that element. When used with css div tags can be styled to create visuals on a web page.
+Span tags create an inline text container. IT IS similar to div, but it's used to apply specific stylings to individual portions of text.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: OOP is programming model that uses object to represent and change data and its behaviour. It organizes around data, and objects rather than functions and logic.
 
-2. Ruby:
+2. Ruby: Ruby is a high level, general purpose programming language created by Yukihiro Matz. It's a language where code is executed without the need for compilation. Everything in ruby is an object. Ruby is typically used with Ruby on Rails.
 
-3. Implicit return:
+3. Implicit return: Implicit return allows us to return values without using the return keyword. When creating an implicit return, you can just use the variable and then parentheses. const('implicit returns') -- which returns whatever string you've implemented.
 
-4. Ruby blocks:
+4. Ruby blocks: A Ruby block can be passed as an argument to a method. block are defined using curly braces, 'do' or 'end' keywords. 
 
-5. Ruby hashes:
+5. Ruby hashes: In Ruby a hash is a collection of key-value pairs
